d8a4020b4e9402d2c1046bc114c3a199fd1be6f9
nabeliwo <nabeliwo@gmail.com>
2024-05-02 10:50:36 +0000

    modify not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,17 @@
+import { Heading } from '@/components/ui/Heading'
+
 export default function NotFound() {
-  return <p>TODO: Not Found</p>
+  return (
+    <>
+      <Heading>404 Not Found</Heading>
+
+      <div className="mt-8">
+        <p>
+          The page you were looking for could not be found.
+          <br />
+          The page you indicated may have been deleted or moved.
+        </p>
+      </div>
+    </>
+  )
 }
