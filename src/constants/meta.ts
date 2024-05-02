160a83ebe0f4f7d29211652cd37e845edf73a585
nabeliwo <nabeliwo@gmail.com>
2024-05-02 06:09:51 +0000

    change url

--- src/constants/meta.ts
+++ src/constants/meta.ts
@@ -1,24 +1,24 @@
 export const profile = {
   siteName: 'nabeliwo hub',
   description: 'The nabeliwo hub is the place where all information about me is gathered.',
-  url: 'https://nabeliwo.com',
+  url: 'https://nabeliwo.blue',
 }
 
 export const blog = {
   siteName: 'nabeliwo blog',
   description:
     'nabeliwo blog は nabeliwo の日々の出来事だったり生活の中で感じたことだったりを雑にお届けします。',
-  url: 'https://nabeliwo.com/blog',
+  url: 'https://nabeliwo.blue/blog',
 }
 
 export const weekly = {
   siteName: 'nabeliwo weekly',
   description: 'nabeliwo weekly は nabeliwo の日々の活動を週報の形式でまとめてお届けします。',
-  url: 'https://nabeliwo.com/weekly',
+  url: 'https://nabeliwo.blue/weekly',
 }
 
 export const bestBuy = {
   siteName: 'nabeliwo best buy',
   description: 'The nabeliwo best buy is a place where I show you my favorite things I bought.',
-  url: 'https://nabeliwo.com/best-buy',
+  url: 'https://nabeliwo.blue/best-buy',
 }
