c5e0a1d89754bddbfc513f25565b8b4f784dd0a0
nabeliwo <nabeliwo@gmail.com>
2024-05-02 16:37:49 +0000

    update redirect setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      // 旧 blog.nabeliwo.com へのアクセスを blog にリダイレクトする
+      {
+        source: '/:year(\\d{4})-:month(\\d{2})-:day(\\d{2})',
+        destination: '/weekly/:year/:month-:day',
+      },
+
+      // 旧 nippo.nabeliwo.com へのアクセスを weekly にリダイレクトする
+      {
+        source: '/:year(\\d{4})/:month(\\d{2})/:slug',
+        destination: '/blog/:year/:month/:slug',
+      },
+    ]
+  },
+}
 
 export default nextConfig
